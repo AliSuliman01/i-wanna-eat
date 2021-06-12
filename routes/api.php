d7cca6283ecbe0074fea24fa40012753f21057ee
AloushA <51234380+AloushA@users.noreply.github.com>
2021-06-12 08:32:38 +0300

    commit 7 12-6-2021 : add server endpoints : show all tables . show table. passport clients . link storage . config clear .

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Schema;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +19,35 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/add_clients', function () {
+
+    Artisan::call('passport:install');
+
+});
+Route::get('/linkstorage', function () {
+
+    Artisan::call('storage:link');
+
+});
+
+Route::get('config-cache',function (){
+    $exitCode = Artisan::call('config:cache');
+    return '<h1>cache config cleared!</h1>';
+});
+
+Route::get('/show_all_tables', function () {
+    $tables = DB::select('SHOW TABLES');
+    echo '[';
+    foreach($tables as $table){
+        print_r($table);
+    }
+    echo ']';
+
+});
+Route::get('/show_table/{table_name}', function ($table_name) {
+
+    return Schema::getColumnListing($table_name);
+
 });
