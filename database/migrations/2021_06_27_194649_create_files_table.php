ebf6637e4d921fccd64a91d89772f510f72139a9
AloushA <51234380+AloushA@users.noreply.github.com>
2021-06-28 11:04:14 +0300

    commit 25 : files migration

--- database/migrations/2021_06_27_194649_create_files_table.php
+++ database/migrations/2021_06_27_194649_create_files_table.php
@@ -16,6 +16,8 @@ class CreateFilesTable extends Migration
         try{
             Schema::create('files', function (Blueprint $table) {
                 $table->id();
+                $table->string('file_name');
+                $table->string('file_path');
                 $table->foreignId('created_by_user_id')->nullable()->constrained('users')->onDelete('cascade');
                 $table->foreignId('updated_by_user_id')->nullable()->constrained('users')->onDelete('cascade');
                 $table->foreignId('deleted_by_user_id')->nullable()->constrained('users')->onDelete('cascade');
