5b6c345b6b47ac82f9afd48d070b24f02a6b6e27
AloushA <51234380+AloushA@users.noreply.github.com>
2021-06-30 01:47:39 +0300

    commit 42 : searching & test auth

---
+++ app/Http/ViewModels/Android/AndroidSearchVM.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace App\Http\ViewModels\Android;
+
+
+use App\Domain\Main\Categories\Categories\Model\Category;
+use App\Domain\Main\Products\Products\Model\Product;
+use App\Domain\Main\Restaurants\Restaurants\Model\Restaurant;
+use Illuminate\Contracts\Support\Arrayable;
+
+class AndroidSearchVM implements Arrayable
+{
+    private $sub_string ;
+
+    public function __construct($props)
+    {
+        $this->sub_string = $props['sub_string'];
+    }
+
+    public function get_restaurants(){
+        return Restaurant::with(['photos'])->where('name','LIKE',"%$this->sub_string%")->get();
+    }
+    public function get_products(){
+        return Product::with(['translations','photos'])->whereHas('translations',function ($translation){
+            $translation->where('name','LIKE',"%$this->sub_string%");
+        })->get();
+    }
+    public function get_categories(){
+        return Category::with(['translations','photos'])->whereHas('translations',function ($translation){
+                $translation->where('name','LIKE',"%$this->sub_string%");
+            })->get();
+    }
+    public function toArray()
+    {
+        return [
+            'restaurants' => $this->get_restaurants(),
+            'products' => $this->get_products(),
+            'categories' => $this->get_categories(),
+        ];
+    }
+}
